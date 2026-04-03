cc34b4bf6e4f4c5b43188bb2e3b48cf061639bed
Adekola Olaonipekun <adekolaolaonipekun@gmail.com>
2026-04-03 23:01:54 +0100

    fixes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://coat-roll.onrender.com"], // TODO: Confirm Render frontend URL
+    origin: ["http://localhost:3000", "https://coat-roll-i4hh.onrender.com"],
     credentials: true,
   })
 );
